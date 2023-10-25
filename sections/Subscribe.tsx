99a3639dd28d2939072a79cb354722bb7c98d075
muhammadbilal10 <muhdbilal81@gmail.com>
2023-10-26 00:01:19 +0500

    feat: Add Subscribe

--- sections/Subscribe.tsx
+++ sections/Subscribe.tsx
@@ -1,5 +1,22 @@
+import Button from "@/components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="flex max-lg:flex-col items-center justify-between gap-10"
+    >
+      <h2 className="text-4xl font-bold text-center">
+        Signup for <span className="text-coral-red">Updates</span> & Newsletter
+      </h2>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="SignUp" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
